66b942b121de39cd411bb09ea88df586cabda0cf
mohamed.el-moukhtari <mohamed.elmoukhtari@prestataire.april.com>
2026-01-01 22:55:16 +0100

    INIT PROJECT MASTER

---
+++ RPA-master/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'standard',
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'no-multi-spaces': 'off',
+    'key-spacing': 'off',
+    'semi': 'off',
+    'indent': 'off',
+    'no-unused-vars': 'off',
+    'camelcase': 'off',
+    'no-debugger': 'off'
+  }
+}
